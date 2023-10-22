0934b0bc0d7e4dfa070aff4e319e5b2eceb2a538
riemer1818 <riemer.vandervliet@Live.nl>
2023-10-22 18:30:12 +0800

    ugh

--- run4.2.sh
+++ run4.2.sh
@@ -3,10 +3,9 @@ export CUBLAS_WORKSPACE_CONFIG=:16:8
 python3 AI6103-HomeworkAssignment/main.py \
 --dataset_dir ./datasets \
 --batch_size 128 \
---epochs 15 \
---lr 0.05 --wd 0.0005 \
+--epochs 300 \
+--lr 0.05 --wd 0 \
 --mixup \
 --seed 0 \
---fig_name lr=0.05-lr_sche-wd=0.0005-mixup.png \
+--fig_name lr=0.05-lr_sche-wd=0-mixup.png \
 --test
-
