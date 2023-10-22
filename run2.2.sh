6df4c5708175e87a237c64e8107ae53ed6a8810f
riemer1818 <riemer.vandervliet@Live.nl>
2023-10-22 16:20:02 +0800

    filler file

--- run2.2.sh
+++ run2.2.sh
@@ -5,7 +5,6 @@ python3 AI6103-HomeworkAssignment/main.py \
 --batch_size 128 \
 --epochs 15 \
 --lr 0.05 --wd 0.0005 \
---lr_scheduler \
 --mixup \
 --seed 0 \
 --fig_name lr=0.05-lr_sche-wd=0.0005-mixup.png \
