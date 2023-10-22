82959902b8495e95addb898589d76135d90608b6
riemer1818 <riemer.vandervliet@Live.nl>
2023-10-22 18:18:29 +0800

    ex 4

--- 4.1.sh
+++ 4.1.sh
@@ -5,7 +5,8 @@ python3 AI6103-HomeworkAssignment/main.py \
 --batch_size 128 \
 --epochs 300 \
 --lr 0.05 --wd 0.0001 \
+--lr_scheduler \
 --mixup \
 --seed 0 \
---fig_name lr=0.05-lr_sche-wd=0.0005-mixup.png \
+--fig_name lr=0.05-lr_sche-wd=0.0001-mixup.png \
 --test
