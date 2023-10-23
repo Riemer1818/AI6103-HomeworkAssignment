0cd66e39c7765c20e060e43b9ced748f173cdab0
riemer1818 <riemer.vandervliet@Live.nl>
2023-10-23 12:17:28 +0800

    quick bugfix

---
+++ run3.1_alpha0.2.sh
@@ -0,0 +1,13 @@
+export CUBLAS_WORKSPACE_CONFIG=:16:8
+
+python3 AI6103-HomeworkAssignment/main.py \
+--dataset_dir ./datasets \
+--batch_size 128 \
+--epochs 15 \
+--lr 0.5 --wd 0 \
+--alpha 0.2 \
+--mixup \
+--seed 0 \
+--fig_name 3.1-lr=0.5-lr_sche-wd=0-mixup_alpha=0.2.png \
+--test
+
