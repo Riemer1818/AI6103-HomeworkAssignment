10bbaf57842cbfb3713dad87900ed8144c6e9191
riemer1818 <riemer.vandervliet@Live.nl>
2023-10-23 12:05:31 +0800

    quick bugfix

--- run3.1_alpha0.sh
+++ run3.1_alpha0.sh
@@ -5,7 +5,7 @@ python3 AI6103-HomeworkAssignment/main.py \
 --batch_size 128 \
 --epochs 15 \
 --lr 0.5 --wd 0 \
---alpha 0.0\
+--alpha 0.0 \
 --mixup \
 --seed 0 \
 --fig_name 3.1-lr=0.5-lr_sche-wd=0-mixup_alpha=0.png \
